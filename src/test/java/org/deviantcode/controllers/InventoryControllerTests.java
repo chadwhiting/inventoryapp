ac9fdbb5164e7714b8773549401ac723fc9037fd
Chad Whiting <cwhiting@expedia.com>
2014-11-30 11:49:02 -0800

    Add basic unit tests for create and update

--- src/test/java/org/deviantcode/controllers/InventoryControllerTests.java
+++ src/test/java/org/deviantcode/controllers/InventoryControllerTests.java
@@ -7,6 +7,7 @@ import org.easymock.IMocksControl;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.testng.annotations.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
@@ -15,7 +16,7 @@ import static org.easymock.EasyMock.*;
 @Test(groups = "unit")
 public class InventoryControllerTests {
     @Test
-    public void testShowAllInventory() {
+    public void testGetAll() {
         IMocksControl mocksControl = EasyMock.createControl();
         final String itemName = "Test Item";
         Item mockItem = mocksControl.createMock(Item.class);
@@ -39,7 +40,7 @@ public class InventoryControllerTests {
     }
 
     @Test
-    public void testGetInventory() {
+    public void testGetInventoryMethod() {
         IMocksControl mocksControl = EasyMock.createControl();
         final String itemName = "Test Item";
         Item mockItem = mocksControl.createMock(Item.class);
@@ -63,7 +64,7 @@ public class InventoryControllerTests {
     }
 
     @Test
-    public void testGetItem() {
+    public void testGet() {
         final String itemName = "Test Item";
         IMocksControl mocksControl = EasyMock.createControl();
 
@@ -83,5 +84,130 @@ public class InventoryControllerTests {
         expected.setName(itemName);
         Item actual = inventoryController.get(itemName);
         assertEquals(actual.getName(), expected.getName());
+    }
+
+    @Test
+    public void testCreate() {
+        final String name = "Test Item";
+        final String descr = "A Test Description";
+        final String manufacturer = "Test Manufacturer";
+        final String make = "Tester";
+        final String model = "Testv1";
+        final String color = "Black";
+        final String serialno = "123456789";
+        final BigDecimal price = new BigDecimal(123.42);
+
+        IMocksControl mocksControl = EasyMock.createControl();
+
+        ItemDAO mockDAO = mocksControl.createMock(ItemDAO.class);
+        expect(mockDAO.get(anyObject(String.class))).andReturn(null);
+        mockDAO.create(
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(BigDecimal.class));
+        expectLastCall();
+
+        Item mockItem = mocksControl.createMock(Item.class);
+        expect(mockItem.getName()).andReturn(name);
+        expect(mockItem.getDescr()).andReturn(descr);
+        expect(mockItem.getManufacturer()).andReturn(manufacturer);
+        expect(mockItem.getMake()).andReturn(make);
+        expect(mockItem.getModel()).andReturn(model);
+        expect(mockItem.getColor()).andReturn(color);
+        expect(mockItem.getSerialNo()).andReturn(serialno);
+        expect(mockItem.getPrice()).andReturn(price);
+
+        expect(mockDAO.get(anyObject(String.class))).andReturn(mockItem);
+
+        mocksControl.replay();
+
+        InventoryController inventoryController = new InventoryController();
+        ReflectionTestUtils.setField(inventoryController, "itemDAO", mockDAO);
+
+        final Item expected = new Item();
+        expected.setName(name);
+        expected.setDescr(descr);
+        expected.setManufacturer(manufacturer);
+        expected.setMake(make);
+        expected.setModel(model);
+        expected.setColor(color);
+        expected.setSerialNo(serialno);
+        expected.setPrice(price);
+        Item actual = inventoryController.create(name, descr, manufacturer, make, model, color, serialno, price);
+        assertEquals(actual.getName(), expected.getName());
+        assertEquals(actual.getDescr(), expected.getDescr());
+        assertEquals(actual.getManufacturer(), expected.getManufacturer());
+        assertEquals(actual.getMake(), expected.getMake());
+        assertEquals(actual.getModel(), expected.getModel());
+        assertEquals(actual.getColor(), expected.getColor());
+        assertEquals(actual.getSerialNo(), expected.getSerialNo());
+        assertEquals(actual.getPrice(), expected.getPrice());
+    }
+
+    @Test
+    public void testUpdate() {
+        final String name = "Test Item";
+        final String descr = "A Test Description";
+        final String manufacturer = "Test Manufacturer";
+        final String make = "Tester";
+        final String model = "Testv1";
+        final String color = "Black";
+        final String serialno = "123456789";
+        final BigDecimal price = new BigDecimal(123.42);
+
+        IMocksControl mocksControl = EasyMock.createControl();
+        Item mockItem = mocksControl.createMock(Item.class);
+        expect(mockItem.getName()).andReturn(name);
+        expect(mockItem.getDescr()).andReturn(descr);
+        expect(mockItem.getManufacturer()).andReturn(manufacturer);
+        expect(mockItem.getMake()).andReturn(make);
+        expect(mockItem.getModel()).andReturn(model);
+        expect(mockItem.getColor()).andReturn(color);
+        expect(mockItem.getSerialNo()).andReturn(serialno);
+        expect(mockItem.getPrice()).andReturn(price);
+
+        ItemDAO mockDAO = mocksControl.createMock(ItemDAO.class);
+        expect(mockDAO.get(anyObject(String.class))).andReturn(mockItem);
+        mockDAO.update(
+                anyObject(Item.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(String.class),
+                anyObject(BigDecimal.class));
+        expectLastCall();
+
+        expect(mockDAO.get(anyObject(String.class))).andReturn(mockItem);
+
+        mocksControl.replay();
+
+        InventoryController inventoryController = new InventoryController();
+        ReflectionTestUtils.setField(inventoryController, "itemDAO", mockDAO);
+
+        final Item expected = new Item();
+        expected.setName(name);
+        expected.setDescr(descr);
+        expected.setManufacturer(manufacturer);
+        expected.setMake(make);
+        expected.setModel(model);
+        expected.setColor(color);
+        expected.setSerialNo(serialno);
+        expected.setPrice(price);
+        Item actual = inventoryController.create(name, descr, manufacturer, make, model, color, serialno, price);
+        assertEquals(actual.getName(), expected.getName());
+        assertEquals(actual.getDescr(), expected.getDescr());
+        assertEquals(actual.getManufacturer(), expected.getManufacturer());
+        assertEquals(actual.getMake(), expected.getMake());
+        assertEquals(actual.getModel(), expected.getModel());
+        assertEquals(actual.getColor(), expected.getColor());
+        assertEquals(actual.getSerialNo(), expected.getSerialNo());
+        assertEquals(actual.getPrice(), expected.getPrice());
     }
 }
